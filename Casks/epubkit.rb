fe72663090985f3fee5811aaae2cd25941f8efe6
fengkx <liangkx8237@gmail.com>
2024-04-28 13:30:03 +0800

    feat: epubkit

--- Casks/epubkit.rb
+++ Casks/epubkit.rb
@@ -4,7 +4,7 @@ cask "epubkit" do
   desc "Best tool to convert web pages to EPUB. Ideal for compiling research, creating personalized reading lists, or archiving websites."
   
   arch arm: "arm64", intel: "x64"
-  version "1.0.13"
+  version "1.0.14"
   
   url "https://github.com/djyde/epubkit-release/releases/download/v#{version}/epubkit-#{version}-#{arch}.dmg"
 
@@ -22,7 +22,6 @@ cask "epubkit" do
     "~/Library/Application Support/epubkit",
     "~/Library/Preferences/org.randynamic.epubkit.plist",
     "~/Library/Saved Application State/org.randynamic.epubkit.savedState",
-
   ]
 
 end
