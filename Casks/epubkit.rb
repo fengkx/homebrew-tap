efe7a76977fbb8b1532a500c363509efecc7e351
fengkx <liangkx8237@gmail.com>
2024-04-28 14:18:23 +0800

    fix: linter

--- Casks/epubkit.rb
+++ Casks/epubkit.rb
@@ -1,27 +1,26 @@
 cask "epubkit" do
-  name "epubkit"
-  homepage "https://epubkit.app"
-  desc "Best tool to convert web pages to EPUB. Ideal for compiling research, creating personalized reading lists, or archiving websites."
-  
   arch arm: "arm64", intel: "x64"
-  version "1.0.14"
-  
-  url "https://github.com/djyde/epubkit-release/releases/download/v#{version}/epubkit-#{version}-#{arch}.dmg"
 
-  app "EpubKit.app"
+  version "1.0.14"
+
+  url "https://github.com/djyde/epubkit-release/releases/download/v#{version}/epubkit-#{version}-#{arch}.dmg"
+  name "epubkit"
+  desc "Best tool to convert web pages to EPUB"
+  homepage "https://epubkit.app/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
+  app "EpubKit.app"
+
   zap trash: [
+    "~/Library/Application Support/epubkit",
     "~/Library/Caches/epubkit-updater",
     "~/Library/Caches/org.randynamic.epubkit",
     "~/Library/Caches/org.randynamic.epubkit.ShipIt",
-    "~/Library/Application Support/epubkit",
     "~/Library/Preferences/org.randynamic.epubkit.plist",
     "~/Library/Saved Application State/org.randynamic.epubkit.savedState",
   ]
-
 end
