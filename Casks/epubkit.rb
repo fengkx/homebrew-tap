f5e31ef561b72523b7687d281fa2debfaea5479a
fengkx <liangkx8237@gmail.com>
2024-05-14 10:55:15 +0800

    Update epubkit.rb

--- Casks/epubkit.rb
+++ Casks/epubkit.rb
@@ -1,7 +1,7 @@
 cask "epubkit" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.14"
+  version "1.2.4"
   sha256 :no_check
 
   url "https://github.com/djyde/epubkit-release/releases/download/v#{version}/epubkit-#{version}-#{arch}.dmg",
